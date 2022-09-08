a543c7e3947ce3fb15caf1b732cc4a0265a8615b
Huy <Huy@gmail.com>
2022-09-08 14:22:06 +0700

    config static files, use css in ejs

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,7 +1,10 @@
 //cấu hình view engine ejs
 import  express  from "express";
+import  path from "path";
 
 const configViewEngine = (app)=>{
+    /* app.use(express.static(path.join(__dirname, 'public'))); */
+    app.use(express.static('./src/public'));//cho phép các file được lưu trữ trong file public sẽ ở chế độ public và có thể xem được ở phía client
     app.set('view engine','ejs');//cấu hình view engine ejs
     app.set('views','./src/views');//tìm file ejs ở file nào
 }
--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const path = require('path');
 /* const express = require('express') */
 import express from 'express';
 import viewEngine from './configs/viewEngine';
+
 require('dotenv').config()//use dotenv 
 
 const app = express()
-const port = process.env.PORT || 3000 //lấy port ở biến môi trường ,||3000 là backup cho lỗi khi kh lấy đưuocj port ở env
+const port = process.env.PORT || 3000 //lấy port ở biến môi trường ,||3000 là backup cho lỗi khi kh lấy được port ở env
 
+//function config view engine
 viewEngine(app)
 
 app.get('/', (req, res) => {
