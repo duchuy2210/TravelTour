048fa55a1501c58e462d9bf8ce2c62c71f13d93c
Huy <Huy@gmail.com>
2022-09-08 13:39:21 +0700

    use dotenv and config it,port

--- src/server.js
+++ src/server.js
@@ -2,15 +2,16 @@ const path = require('path');
 /* const express = require('express') */
 import express from 'express';
 import viewEngine from './configs/viewEngine';
+require('dotenv').config()//use dotenv 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000 //lấy port ở biến môi trường ,||3000 là backup cho lỗi khi kh lấy đưuocj port ở env
+
+viewEngine(app)
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
 })
-
-viewEngine(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
