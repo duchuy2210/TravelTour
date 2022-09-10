f0eff7ecc1e233ed725ae9287700f00976f06465
Huy <Huy@gmail.com>
2022-09-10 16:52:41 +0700

    update

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,7 +1,7 @@
 import connection from "../../configs/connectDB.js";
 class homeConttroller {
     getHomePage(req, res, next) { 
-        let data = [];
+        /* let data = [];
         connection.query(
             'SELECT * FROM `users`',
             function(err, results, fields) {
@@ -16,10 +16,10 @@ class homeConttroller {
                 })
                 console.log(data);
                 return res.render('index.ejs',{dataUser: JSON.stringify(data)})
-            });
+            }); */
     }
     duchuy(req, res, next) {
-        res.render('duchuy')
+        res.render('homePage.ejs')
     }
 }
 
--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,11 +1,13 @@
 //cấu hình view engine ejs
 import  express  from "express";
+import expressLayouts from 'express-ejs-layouts'
 
 const configViewEngine = (app)=>{
-
+    app.use(expressLayouts);
     app.use(express.static('./src/public'));//cho phép các file được lưu trữ trong file public sẽ ở chế độ public và có thể xem được ở phía client
     app.set('view engine','ejs');//cấu hình view engine ejs
     app.set('views','./src/resources/views');//tìm file ejs ở file nào
-    console.log(__dirname);//
+    console.log(__dirname);
+    app.set('layout','./src/resources/layouts/home.ejs')
 }
 export default configViewEngine;
