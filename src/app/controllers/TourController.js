ae393a47cc6aa76a90d5164d4b9440d5f078546e
duchuy <dhuy221001@gmail.com>
2022-09-19 15:24:21 +0700

    create deletePage,editPage and config their route

--- src/app/controllers/TourController.js
+++ src/app/controllers/TourController.js
@@ -17,9 +17,25 @@ class tourController{
     //[POST]    /create_new_page/
     async createNewTour(req,res,next){
         const {tourname, time, cost, route} = req.body;//khi dùng kiểu này thì phải khai báo trong mảng giống name khi post lên
-        await pool.execute('INSERT INTO users(tourname,time,cost,route) VALUES (?,?,?,?)',[tourname, time, cost, route]);
-        console.log({tourname,time,cost,route});
+        await pool.execute('INSERT INTO users(tourname,time,cost,route) VALUES (?,?,?,?)',[tourname , time, cost, route]);
         res.redirect('/');
+    }
+    //[POST] /tours//delete_tour
+    async deleteTour(req, res, next) {
+        await pool.execute('DELETE FROM `users` where id = ?',[req.body.tourId])
+        res.redirect('/');
+    }
+    //[GET] /tours//edit_tour/:id
+    async getEditPage(req,res,next){
+        const id = req.params.id;
+        const [tour] = await pool.execute('SELECT * FROM `users` where id = ?',[id])
+        res.render('editPage',{tour:tour[0]});
+    }
+    //[POST] /tours/edit_tour_complete
+    async editTour(req, res, next){
+        const {tourname, time, cost, route, id} = req.body;
+        await pool.execute('UPDATE `users` SET `tourName` = ? ,`time` = ?, `cost` = ? ,`route` = ? WHERE id = ?',[tourname,time,cost,route,id]);
+        res.redirect('/')
     }
 }
 export default new tourController;
--- src/routes/tours.js
+++ src/routes/tours.js
@@ -4,6 +4,9 @@ import tourControler from '../app/controllers/TourController.js'
 
 Router.get('/create_page',tourControler.getCreatePage);
 Router.post('/create_new_tour',tourControler.createNewTour);
+Router.post('/delete_tour',tourControler.deleteTour)
 Router.get('/detail_tour/:id',tourControler.getDetailPage);
+Router.get('/edit_tour/:id',tourControler.getEditPage);
+Router.post('/edit_tour_complete',tourControler.editTour);
 
 export default Router;
